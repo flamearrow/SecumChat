1c5466ebeda87e77dd2f84a16178fd39d6362d09
Chen Cen <arrow.cen@gmail.com>
2017-03-27 23:08:34 -0700

    update package

--- app/src/androidTest/java/com/shanjingtech/secumchat/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/shanjingtech/secumchat/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("shanjingtech.com.secumchat", appContext.getPackageName());
+        assertEquals("com.shanjingtech.secumchat", appContext.getPackageName());
     }
 }

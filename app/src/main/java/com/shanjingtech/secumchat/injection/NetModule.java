4ebb838b260c8cb5d4c589f92eb2369bcad99bf6
Chen Cen <arrow.cen@gmail.com>
2018-07-30 23:50:53 -0700

    temporarily disable ssl check to get server response

--- app/src/main/java/com/shanjingtech/secumchat/injection/NetModule.java
+++ app/src/main/java/com/shanjingtech/secumchat/injection/NetModule.java
@@ -26,9 +26,16 @@ import org.webrtc.PeerConnectionFactory;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 
 import javax.inject.Singleton;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import dagger.Module;
 import dagger.Provides;
@@ -36,6 +43,7 @@ import okhttp3.Authenticator;
 import okhttp3.Cache;
 import okhttp3.Credentials;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import retrofit2.Converter;
@@ -78,45 +86,138 @@ public class NetModule {
         return gsonBuilder.create();
     }
 
+    // TODO: load the certificate file and add the trusted managers
+    // https://developer.android.com/training/articles/security-ssl#SelfSigned
+    // https://stackoverflow.com/questions/29273387/certpathvalidatorexception-trust-anchor-for
+    // -certificate-path-not-found-retro
+//    @Provides
+//    @Singleton
+//    SSLSocketFactory provideSSLSocketFactory() throws CertificateException, IOException,
+//            NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+//        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+//        InputStream cert = context.getResources().openRawResource(R.raw.my_cert);
+//        Certificate ca;
+//        try {
+//            ca = cf.generateCertificate(cert);
+//        } finally {
+//            cert.close();
+//        }
+//
+//        // creating a KeyStore containing our trusted CAs
+//        String keyStoreType = KeyStore.getDefaultType();
+//        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+//        keyStore.load(null, null);
+//        keyStore.setCertificateEntry("ca", ca);
+//
+//        // creating a TrustManager that trusts the CAs in our KeyStore
+//        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+//        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+//        tmf.init(keyStore);
+//
+//        // creating an SSLSocketFactory that uses our TrustManager
+//        SSLContext sslContext = SSLContext.getInstance("TLS");
+//        sslContext.init(null, tmf.getTrustManagers(), null);
+//
+//        return sslContext.getSocketFactory();
+//    }
+
+    // TODO: This returns a trustmanager that disabled certification, should NOT be used
+    // https://stackoverflow.com/questions/2893819/accept-servers-self-signed-ssl-certificate-in
+    // -java-client
     @Provides
     @Singleton
-    OkHttpClient provideOkHttpClient(Cache cache, Authenticator authenticator) {
-        return new OkHttpClient.Builder().cache(cache).authenticator(authenticator).build();
+    SSLSocketFactory provideDisablingSSLSocketFactory() {
+        TrustManager[] trustAllCertsTrustManagers = new TrustManager[]{
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+
+                    public void checkClientTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCertsTrustManagers, null);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        assert sslContext != null;
+        return sslContext.getSocketFactory();
+
+    }
+
+//    @Provides
+//    @Singleton
+//    X509TrustManager provideX509TrustManager() {
+//        // TODO: wtf should I return here
+//        return null;
+//    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(Cache cache,
+                                     Authenticator authenticator,
+                                     SharedPreferences sharedPreferences
+            , SSLSocketFactory unSafeSocketFactory
+//                                     X509TrustManager x509TrustManager
+    ) {
+        // Interceptor modifies outgoing request and touches incoming response
+        // chain.proceed() call is blocking and waits the response
+        return new OkHttpClient.Builder().cache(cache).authenticator(authenticator)
+                .addInterceptor(chain -> {
+                    Response response = chain.proceed(addHeaderToRequest(chain.request(),
+                            sharedPreferences));
+                    return response;
+                })
+                .sslSocketFactory(unSafeSocketFactory)
+                .build();
+    }
+
+
+    private Request addHeaderToRequest(Request request, SharedPreferences sharedPreferences) {
+        String credential;
+        if (shouldUseBasicCredential(request.url().toString())) {
+            credential = Credentials.basic(SecumAPI.USER_NAME, SecumAPI.PASSWORD);
+        } else {
+            if (SecumDebug.isDebugMode(sharedPreferences)) {
+                // if it's debug mode, hardcode 11 or 22's credential
+                switch (SecumDebug.getCurrentDebugUser(sharedPreferences)) {
+                    case SecumDebug.USER_11:
+                        credential = "Bearer " + SecumDebug.TOKEN_11;
+                        break;
+                    case SecumDebug.USER_22:
+                        credential = "Bearer " + SecumDebug.TOKEN_22;
+                        break;
+                    default:
+                        credential = "mlgb";
+                        Log.d(TAG, "No debug user credential found");
+                }
+            } else {
+                String bearer = sharedPreferences.getString(Constants
+                        .SHARED_PREF_ACCESS_TOKEN, "mlgb");
+                credential = "Bearer " + bearer;
+                Log.d(TAG, "Bearer: " + bearer);
+            }
+        }
+        // TODO: when token expires, fail fast
+        return request.newBuilder()
+                .header("Authorization", credential)
+                .build();
     }
 
     @Provides
     @Singleton
     Authenticator provideAuthenticator(final SharedPreferences sharedPreferences) {
-        return (route, response) -> {
-            String credential;
-            if (shouldUseBasicCredential(response)) {
-                credential = Credentials.basic(SecumAPI.USER_NAME, SecumAPI.PASSWORD);
-            } else {
-                if (SecumDebug.isDebugMode(sharedPreferences)) {
-                    // if it's debug mode, hardcode 11 or 22's credential
-                    switch (SecumDebug.getCurrentDebugUser(sharedPreferences)) {
-                        case SecumDebug.USER_11:
-                            credential = "Bearer " + SecumDebug.TOKEN_11;
-                            break;
-                        case SecumDebug.USER_22:
-                            credential = "Bearer " + SecumDebug.TOKEN_22;
-                            break;
-                        default:
-                            credential = "mlgb";
-                            Log.d(TAG, "No debug user credential found");
-                    }
-                } else {
-                    String bearer = sharedPreferences.getString(Constants
-                            .SHARED_PREF_ACCESS_TOKEN, "mlgb");
-                    credential = "Bearer " + bearer;
-                    Log.d(TAG, "Bearer: " + bearer);
-                }
-            }
-            // TODO: when token expires, fail fast
-            return response.request().newBuilder()
-                    .header("Authorization", credential)
-                    .build();
-        };
+        return (route, response) ->
+                addHeaderToRequest(response.request(), sharedPreferences);
     }
 
     /**
@@ -124,11 +225,10 @@ public class NetModule {
      * For register/getAccessCode/getAccessToken, use basic
      * For others(ping/getMatch/endMatch/updateUser), use oauth
      *
-     * @param response
+     * @param url
      * @return
      */
-    private boolean shouldUseBasicCredential(Response response) {
-        String url = response.request().url().toString();
+    private boolean shouldUseBasicCredential(String url) {
         // basic register/getAccessCode/getAccessToken
         return url.endsWith(Constants.PATH_REGISTER_USER) || url.endsWith(Constants
                 .PATH_GET_ACCESS_CODE) || url.endsWith(Constants.PATH_GET_ACCESS_TOKEN);
@@ -153,12 +253,25 @@ public class NetModule {
 
     @Provides
     @Singleton
-    public Retrofit provideRetrofit(Gson gson, OkHttpClient okHttpClient) {
+    public Retrofit provideRetrofit(OkHttpClient okHttpClient) {
         // might not need client
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(apiBaseUrl)
                 .client(okHttpClient)
-                .addConverterFactory(new NullOnEmptyConverterFactory())
+                .addConverterFactory(new Converter.Factory() {
+                    @Override
+                    public Converter<ResponseBody, ?> responseBodyConverter(Type type,
+                                                                            Annotation[]
+                                                                                    annotations,
+                                                                            Retrofit retrofit) {
+                        final Converter<ResponseBody, ?> delegate = retrofit
+                                .nextResponseBodyConverter(this, type, annotations);
+                        return (Converter<ResponseBody, Object>) body -> {
+                            if (body.contentLength() == 0) return null;
+                            return delegate.convert(body);
+                        };
+                    }
+                })
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit;
